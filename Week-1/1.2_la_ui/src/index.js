2fb8b0987998a4d4d880c6f4b8548acb8aeb8056
Kiran R <kiranjolisa@gmail.com>
2023-03-12 21:27:08 +0530

    implemented UI for music player

--- Week-1/1.2_la_ui/src/index.js
+++ Week-1/1.2_la_ui/src/index.js
@@ -1,1 +1,37 @@
-console.log('init config')
+const tracksContent = [
+    {
+        name: 'Dwell', 
+        album: 'Heaven',
+        duration: '2:47',
+    },
+    {
+        name: 'Dwell', 
+        album: 'Heaven',
+        duration: '2:47',
+    },
+    {
+        name: 'Dwell', 
+        album: 'Heaven',
+        duration: '2:47',
+    },
+    {
+        name: 'Dwell', 
+        album: 'Heaven',
+        duration: '2:47',
+    }, 
+    {
+        name: 'Dwell', 
+        album: 'Heaven',
+        duration: '2:47',
+    }
+]
+
+tracksContent.forEach((track, i) => {
+    document.getElementById('next-tracks-list').innerHTML += `
+    <li>
+        <p>${i+1}. ${track.name}</p>
+        <p>${track.album}</p>
+        <p>${track.duration}</p>
+        <a href='#'>CATTY CLOUD SYNC</a>
+    </li>`;
+})
